6cd74c3625242b6824bdc919ebee64af63dc9e8c
wahyu aris munandar <wahyuam007@gmail.com>
2020-12-24 11:07:53 +0700

    loading overlay on click

--- js/login.js
+++ js/login.js
@@ -12,6 +12,7 @@ btnSubmit.addEventListener('click', () => {
 });
 
 let login = () => {
+    var spinHandle = loadingOverlay.activate();
     let dataUSer = {
         "username" : document.getElementById("username").value,
         "password" : document.getElementById("password").value
@@ -34,10 +35,12 @@ let login = () => {
             console.log("go to index.html");
             window.location.href = "index.html";
         } else {
+            loadingOverlay.cancel(spinHandle);
             console.log("back to login.html");
             alertInfo.classList.remove("hide");
         }})
     .catch(err => {
+        loadingOverlay.cancel(spinHandle);
         console.log(err);
         alertInfo.innerHTML="ERR_CONNECTION_REFUSED PLEASE TRY LATER";
         alertInfo.classList.remove("hide");
